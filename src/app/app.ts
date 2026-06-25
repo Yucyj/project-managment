a7a2d544ceccccc6c519551bd3283fd08073ff35
Yucyj <105999968+Yucyj@users.noreply.github.com>
2026-06-25 11:12:16 +0300

    Upload functional angular source code and configurations

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+})
+export class AppComponent {
+  title = "project management";
+}
+
+
