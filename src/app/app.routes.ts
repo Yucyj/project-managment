f536769cd1a609ea477836077dcb0af3df552b85
Yucyj <105999968+Yucyj@users.noreply.github.com>
2026-06-29 12:08:52 +0300

    Implemented auth/login routing pat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  { path: 'auth/login', component: AppComponent }
+];
--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,3 @@ import { AppComponent } from './app/app';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-
